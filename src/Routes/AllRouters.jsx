52fdce49727897a046566f8caab68a1c1d4b638f
Rajatsinha05 <rajatsinha5467@gmail.com>
2023-02-23 20:22:40 +0530

    priavte routing working

--- src/Routes/AllRouters.jsx
+++ src/Routes/AllRouters.jsx
@@ -1,14 +1,33 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Register from "../Pages/Register";
 import Dashborad from "../Pages/Dashborad";
 import Login from "../Pages/Login";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function AllRouters() {
+  const [auth, setAuht] = useState(false);
+const dispatch = useDispatch()
+const nav=useNavigate()
+  const data = useSelector((store) => store.data);
+  useEffect(() => {
+    return () => {
+      if (data.length > 0 && data[data.length - 1].details.auth === true) {
+
+        console.log(data[data.length - 1].details.auth)
+        setAuht(true);
+      }
+    };
+  }, [])
+
   return (
     <Routes>
         <Route path="/register" element={<Register />}/>
-      <Route path="/" element={<Dashborad />}/>
+        <Route path="/" element={auth? <Dashborad/>:<Register/>}/> 
+
+
+
       <Route path="/login" element={<Login/>}/>
     </Routes>
   );
