92d7d883789a65d5dcd8c8a2cdd05942e18b552d
Rajatsinha05 <rajatsinha5467@gmail.com>
2023-02-23 19:55:24 +0530

    login and logout function are working

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,55 @@
 import React, { useState } from "react";
 import "../Pages/pages.css";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { Logout } from "../Redux/Reducer";
 function Navbar() {
   const [auth, setAuht] = useState(false);
-
+const dispatch = useDispatch()
+const nav=useNavigate()
   const data = useSelector((store) => store.data);
-// if(data.length>0) {
+  // if(data.length>0) {
 
-//   console.log(data[data.length - 1].details.auth);
-//   setAuht(data[data.length - 1].details.auth);
-// }
+  //   console.log(data[data.length - 1].details.auth);
+  //   setAuht(data[data.length - 1].details.auth);
+  // }
+
+
+  const handle=()=>{
+   dispatch(Logout(data.length - 1))
+   setTimeout(() => {
+    nav('/login')
+   }, 2000)
+   
+  
+
+  }
+
+  useEffect(() => {
+    return () => {
+      if (data.length > 0 && data[data.length - 1].details.auth === true) {
+
+        console.log(data[data.length - 1].details.auth)
+        setAuht(true);
+      }
+    };
+  }, [])
 
   return (
     <div id="nav">
       <Link to="/">
         <p>Dashborad</p>
       </Link>
-{/* 
-     {auth ?  <p onClick={()=>{}}> Logout</p> :<Link to="/login">
+      
+     {auth ?  <p onClick={handle}> Logout</p> :<Link to="/login">
         <p>Login</p>
       </Link>
 
 
 
-  } */}
- <p>Login</p>
+  }
+
 
       <Link to="/register">
         <p>Register</p>
